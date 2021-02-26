e0e4899c8cbde409d4323356c23d020c2aba51aa
Cispia <70313509+AlineWagner@users.noreply.github.com>
2021-02-26 11:45:11 +0100

    Update

--- SQL/filrouge_employees.sql
+++ SQL/filrouge_employees.sql
@@ -8,13 +8,17 @@ DROP TABLE IF EXISTS `employees`;
 CREATE TABLE IF NOT EXISTS `employees`(
    `emp_id` INT NOT NULL,
    `pos_id` INT NOT NULL,
-   `emp_ref` VARCHAR(10) NOT NULL,
+   `emp_ref` VARCHAR(15) NOT NULL,
    `emp_surname` VARCHAR(50) NOT NULL,
    `emp_name` VARCHAR(50) NOT NULL,
-    PRIMARY KEY (`subcat_id`),
-    FOREIGN KEY `pos_id` REFERENCES Positions (`pos_id`)
+    PRIMARY KEY (`emp_id`),
+    FOREIGN KEY (pos_id) REFERENCES Positions (`pos_id`)
 );
 
 
 INSERT INTO `employees` (`emp_id`, `pos_id`, `emp_ref`, `emp_surname`, `emp_name`) VALUES
-(1, '', '', '', ''),
+(1, 1, 'Cispia', 'Wagner', 'Aline'),
+(2, 2, 'Headmaster', 'Dumbledore', 'Albus'),
+(3, 4, 'PrepareToDie', 'Montoya', 'Inigo'),
+(4, 7, 'XX121', 'Ripley', 'Ellen'),
+(5, 8, 'The Hair', 'Harrington', 'Steve');
---
+++ SQL/filrouge_positions.sql
@@ -0,0 +1,25 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+DROP TABLE IF EXISTS `positions`;
+CREATE TABLE IF NOT EXISTS `positions`(
+   `pos_id` INT NOT NULL,
+   `pos_name` VARCHAR(50) NOT NULL,
+    PRIMARY KEY (`pos_id`)
+);
+
+
+INSERT INTO `positions` (`pos_id`, `pos_name`) VALUES
+(1, 'Web Admin'),
+(2, 'Directeur'),
+(3, 'Service Comptable'),
+(4, 'Ressources Humaines'),
+(5, 'Fonction Production et Développement'),
+(6, 'Fonction Marketing'),
+(7, 'Fonction Communication'),
+(8, 'Vendeur'),
+(9, 'Truc Muche'),
+(10, 'Machin Chouette');
